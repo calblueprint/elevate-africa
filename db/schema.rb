991a9bc9ce98bb8b45c4d6e2659c92035dfcdb55
Charles Xue <charlesx2013@gmail.com>
2014-10-06 12:25:44 -0700

    changed table name

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006065921) do
+ActiveRecord::Schema.define(version: 20141006192007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: true do |t|
+  create_table "buzzes", force: true do |t|
     t.text     "content"
     t.integer  "admin_id"
     t.string   "video_link"
