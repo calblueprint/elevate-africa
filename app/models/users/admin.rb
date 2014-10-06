2cdef6c44168ed6760f511ad0983c9077709b039
Charles Xue <charlesx2013@gmail.com>
2014-10-05 23:01:01 -0700

    added admin user

---
+++ app/models/users/admin.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)
+#  first_name :string(255)
+#  last_name  :string(255)
+#  password   :string(255)
+#  username   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Admin < User
+end
---
+++ app/models/users/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)
+#  first_name :string(255)
+#  last_name  :string(255)
+#  password   :string(255)
+#  username   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+end
