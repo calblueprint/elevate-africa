6a9f4cba989942e79fc0e2962a92d0502422b807
warrenshen <warrenzshen@gmail.com>
2014-10-05 21:00:38 -0700

    make about framework

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
+  get 'about' => 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
