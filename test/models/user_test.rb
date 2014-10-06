64821ac3bf392fc03ad9fad8927d7a4ffbf2f7a4
Charles Xue <charlesx2013@gmail.com>
2014-10-05 22:43:59 -0700

    annotations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)
+#  first_name :string(255)
+#  last_name  :string(255)
+#  password   :string(255)
+#  username   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
