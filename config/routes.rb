40d00de8661a094386b289c5ba92a0300662326d
Charles Xue <charlesx2013@gmail.com>
2014-10-06 12:34:13 -0700

    added routes, removed tests

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   get 'campaign' => 'static_pages#campaign'
 
+  resources :buzzes
+
+  resources :users
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
