ee0ee6e80dc0c4b053b3c755d5af2f0f78e4d48d
Charles Xue <charlesx2013@gmail.com>
2014-10-07 02:53:24 -0700

    controller methods

--- app/controllers/buzzes_controller.rb
+++ app/controllers/buzzes_controller.rb
@@ -1,4 +1,16 @@
 class BuzzesController < ApplicationController
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :destroy]
+
   def index
+  end
+
+  def new
+    @buzz = Buzz.new
+  end
+
+  private
+
+  def authenticate_admin!
+    current_user.is_admin?
   end
 end
