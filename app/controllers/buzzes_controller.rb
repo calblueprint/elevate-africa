05ee36dce6df393bd3ef2f070758167f78f8dee1
Charles Xue <charlesx2013@gmail.com>
2014-10-07 03:13:21 -0700

    added create method

--- app/controllers/buzzes_controller.rb
+++ app/controllers/buzzes_controller.rb
@@ -8,9 +8,24 @@ class BuzzesController < ApplicationController
     @buzz = Buzz.new
   end
 
+  def create
+    @buzz = Buzz.new buzz_params
+    if @buzz.save
+      flash[:success] = "You've created a buzz post!"
+      redirect_to buzzes_path
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def authenticate_admin!
     current_user.is_admin?
+  end
+
+  def buzz_params
+    params.require(:buzz).permit(:title, :subtitle, :content,
+                                 :admin_id, :video_link)
   end
 end
