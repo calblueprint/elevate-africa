ca74a72ad46c68d1f8d193ca416b9042a0bfb94f
Charles Xue <charlesx2013@gmail.com>
2014-10-07 03:47:21 -0700

    moved time stamps

--- db/migrate/20141006065405_create_posts.rb
+++ db/migrate/20141006065405_create_posts.rb
@@ -1,13 +1,12 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.timestamps
       t.text :content
       t.belongs_to :user
       t.string :video_link
       t.string :title
       t.string :subtitle
-
-      t.timestamps
     end
   end
 end
