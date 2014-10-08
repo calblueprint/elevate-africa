2dc7999a4063a3c80322d886dd25177016c50514
Charles Xue <charlesx2013@gmail.com>
2014-10-08 01:12:34 -0700

    fixed the upload problem

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,4 +5,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
   config.fog_directory = ENV["AWS_S3_BUCKET"]
+  # config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end
