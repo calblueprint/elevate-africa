9c9bdcf337c9ae5876f00a712fc85c0135e70053
warrenshen <warrenzshen@gmail.com>
2014-10-13 18:43:06 -0700

    legit centering of process text

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  $(".about-process").on("mouseenter", "img", function() {
+  $(".about-process-container").on("mouseenter", "img", function() {
   //$(this).animate({"opacity": "0"}, "slow");
-    $(this).closest(".about-process").find(".about-process-text").animate({"opacity": "0"}, "slow");
+  	console.log("hello");
+    $(".about-process-text").animate({"opacity": "1"}, "fast");
   });
-  $(".about-process").on("mouseleave", "img", function() {
+  $(".about-process-container").on("mouseleave", "img", function() {
   //$(this).animate({"opacity": "1"}, "slow");
-  	$(this).closest(".about-process").find(".about-process-text").animate({"opacity": "1"}, "slow");
+  	$(".about-process-text").animate({"opacity": "0"}, "fast");
   });
 });
