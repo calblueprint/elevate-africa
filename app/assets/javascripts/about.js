cce99aeae527e3b49c086a5af9b2c6bd8f8864cb
warrenshen <warrenzshen@gmail.com>
2014-10-15 18:34:50 -0700

    text change and refactoring

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   $(".about-circle-container").on("mouseenter", ".about-process-icon", function() {
-  //$(this).animate({"opacity": "0"}, "slow");
-  	console.log("hello");
-  	$(".about-process-text").stop([true][true]);
-    $(".about-process-text").animate({"opacity": "0"}, "slow");
+  	endProcessAnimation();
+    if($(this).hasClass("deg0")) {
+      console.log("deg0 doeeeeee")
+      $(".about-process-text").find(".hi").text("deg0");
+    }
+    $(".about-process-text").animate({"opacity": "1"}, "slow");
   });
+  
   $(".about-circle-container").on("mouseleave", ".about-process-icon", function() {
-  //$(this).animate({"opacity": "1"}, "slow");
-  	$(".about-process-text").stop([true][true]);
-  	$(".about-process-text").animate({"opacity": "1"}, "slow");
+  	endProcessAnimation();
+  	$(".about-process-text").animate({"opacity": "0"}, "slow");
   });
 });
+
+function endProcessAnimation() {
+  $(".about-process-text").stop([true][true]);
+}
 
 function smoothScroll(section) {
   $('html, body').animate({scrollTop: $('#' + section).offset().top}, 1000);
