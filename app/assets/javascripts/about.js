7a693eafdacfdb661cfca22bddeaf0ba8809faa2
warrenshen <warrenzshen@gmail.com>
2014-10-19 20:29:58 -0700

    image assets

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -13,7 +13,7 @@ var ready = function() {
       if(!selected.is(previous)) {
         endProcessAnimation();
     //  fadeOthers(selected, 0.75);
-        fadeGirls();
+        // fadeGirls();
         content.animate({opacity: 0}, function() {
           changeCenterText(selected, content);
           content.animate({opacity: 1});
@@ -89,68 +89,3 @@ function fadeGirls() {
 function smoothScroll(section) {
   $('html, body').animate({scrollTop: $('#' + section).offset().top + 18}, 1000);
 }
-
-// var ready = function() {
-//   $(document).ready(function() {
-//     var circle = $(".about-process-circle")
-//     var content = $(".about-process-text")
-//     circle.on("mouseenter", ".about-process-icon", function() {
-//       var selected = $(this);
-//       endProcessAnimation();
-//       changeCenterText(selected, content);
-//       fadeOthers(selected, 0.75);
-//       content.animate({opacity: 1}, "fast");
-//       $(".about-process-girl-left").animate({"opacity": 1}, "fast");
-//       $(".about-process-girl-right").animate({"opacity": 1}, "fast");
-//       selected.animate({width: "7.5em", height: "7.5em", margin: "-3.75em"}, "fast");
-//     });
-
-//     circle.on("mouseleave", ".about-process-icon", function() {
-//     	var selected = $(this);
-//       endProcessAnimation();
-//       fadeOthers(selected, 1.0);
-//     	content.animate({"opacity": 0}, "fast");
-//       $(".about-process-girl-left").animate({"opacity": 0}, "fast");
-//       $(".about-process-girl-right").animate({"opacity": 0}, "fast");
-
-//       selected.animate({width: "7em", height: "7em", margin: "-3.5em"}, "fast");
-//     });
-//   });
-// }
-
-// $(document).ready(ready);
-// $(document).on('page:load', ready);
-
-// function endProcessAnimation() {
-//   $(".about-process-text").stop(true, true);
-//   $(".about-process-icon").stop(true, true);
-//   $(".about-process-arrow").stop(true, true);
-//   $(".about-process-girl-left").stop(true, true);
-//   $(".about-process-girl-right").stop(true, true);
-// }
-
-// function changeCenterText(selected, content) {
-//   var title = content.find(".about-process-text-title");
-//   var paragraph = content.find(".about-process-text-paragraph");
-
-//   if(selected.hasClass("deg0")) { title.text("strong"); }
-//   else if(selected.hasClass("deg60")) { title.text("doee"); }
-//   else if(selected.hasClass("deg120")) { title.text("super"); }
-//   else if(selected.hasClass("deg180")) { title.text("man"); }
-//   else if(selected.hasClass("deg240")) { title.text("doee"); }
-//   else { title.text("sooo"); }
-// }
-
-// function fadeOthers(selected, opacity) {
-//   $(".about-process-icon").not(selected).each(function() {
-//         $(this).animate({"opacity": opacity}, "fast");
-//       });
-//   $(".about-process-arrow").each(function() {
-//       $(this).animate({"opacity": opacity}, "fast");
-//   });
-// }
-
-// function smoothScroll(section) {
-//   $('html, body').animate({scrollTop: $('#' + section).offset().top + 18}, 1000);
-// }
-
