ab600a6d6d5074b81c61c08ea18f38134b61f291
Charles Xue <charlesx2013@gmail.com>
2014-10-23 23:05:02 -0700

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def after_sign_in_path_for(user)
-  #   TODO: redirect user to campaign if he/she is creating one.
-  # end
+  def after_sign_in_path_for(user)
+    if user.is_admin?
+      redirect_to root_path
+    else
+      # Fix This
+      redirect_to root_path
+    end
+  end
 end
