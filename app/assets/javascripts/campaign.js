6d95b4f9ae18d53a7db1b9098131c4be7f384965
Tricia Fu <triciasfu@gmail.com>
2014-10-23 22:30:24 -0700

    models

--- app/assets/javascripts/campaign.js
+++ app/assets/javascripts/campaign.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-    window.sr = new scrollReveal();
-});
+var ready_campaign = function() {
+    if ($("#campaign-banner").length > 0) {
+        $(document).ready(function() {
+            window.sr = new scrollReveal();
+        });
+    }
+}    
+
+$(document).on('page:load', ready_campaign);
