d101cbc3cfba107a7b9d120c1b1edfda640e7236
warrenshen <warrenzshen@gmail.com>
2014-10-26 20:40:55 -0700

    add columns to campaigns

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   resources :buzzes
 
   resources :users
+
+  resources :campaigns
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024063536) do
+ActiveRecord::Schema.define(version: 20141027031856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20141024063536) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "donation_id"
+    t.string   "description"
+    t.string   "deadline"
   end
 
   add_index "campaigns", ["donation_id"], name: "index_campaigns_on_donation_id", using: :btree
