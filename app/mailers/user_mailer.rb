acbf607c58a65cf599a4f2f7153c7881a520790e
Tricia Fu <triciasfu@gmail.com>
2014-10-28 01:01:10 -0700

    blah

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def contact_email
-    mail(to: params[:'contact-email'], subject: params[:'contact-subject'])
+    mail(to: params[:contact_email], subject: params[:contact_subject])
   end
 end
