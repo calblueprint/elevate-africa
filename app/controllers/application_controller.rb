a55794a459adfa69b0021307d91b88c20b1c02c2
Charles Xue <charlesx2013@gmail.com>
2014-10-30 03:29:31 -0700

    devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     if user.admin?
       root_path
-    # else
-    #   Fix This
-    #   redirect_to root_path
+    elsif user.team?
+      campaign_path user.campaign
     end
   end
 end
