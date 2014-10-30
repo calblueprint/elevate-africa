c9338e947538ba7e14e6dcb974e1f9b9797d4795
Charles Xue <charlesx2013@gmail.com>
2014-10-30 03:45:37 -0700

    working on crud

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,11 +9,26 @@ class TeamsController < ApplicationController
   end
 
   def edit
+    @team = Team.find params[:id]
   end
 
   def update
+    @team = Team.find params[:id]
+    if @team.update_attributes(team_params)
+      flash[:success] = "You've updated your team!"
+      redirect_to team_path(@team)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :email, :password,
+                                 :password_confirmation, :current_password)
   end
 end
