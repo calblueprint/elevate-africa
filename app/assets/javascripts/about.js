68027ca8ca5f0c01cd094085c37f8534c7e1d6ac
warrenshen <warrenzshen@gmail.com>
2014-10-30 20:32:44 -0700

    about graphics and tweaks

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -2,7 +2,7 @@ var ready = function() {
   $(document).ready(function() {
 
     if($(".about-page").length > 0) {
-      var opacity = 0.625;
+      var opacity = 0.75;
       var circle = $(".about-process-circle");
       var content = $(".about-process-text");
 
@@ -20,8 +20,8 @@ var ready = function() {
             changeCenterText(selected, content);
             content.animate({opacity: 1});
           });
-          selected.animate({width: "7em", height: "7em", margin: "-3.5em", opacity: 1});
-          previous.animate({width: "6em", height: "6em", margin: "-3em", opacity: opacity});
+          selected.animate({width: "7.5em", height: "7.5em", margin: "-3.75em", opacity: 1});
+          previous.animate({width: "7em", height: "7em", margin: "-3.5em", opacity: opacity});
           previous = selected;
         }
       });
@@ -43,12 +43,8 @@ function initialSettings(first_icon, content, opacity) {
   $(".about-process-icon").not(first_icon).each(function() {
     $(this).css({"opacity": opacity});
   });
-
-  $(".about-process-arrow").each(function() {
-    $(this).css({"opacity": opacity});
-  });
   
-  first_icon.css({width: "7em", height: "7em", margin: "-3.5em"});
+  first_icon.css({width: "7.5em", height: "7.5em", margin: "-3.75em"});
   changeCenterText(first_icon, content);
 }
 
@@ -56,23 +52,14 @@ function changeCenterText(selected, content) {
   var title = content.find(".about-process-text-title");
   var paragraph = content.find(".about-process-text-paragraph");
 
-  if(selected.hasClass("icon-one")) { title.text("put"); }
-  else if(selected.hasClass("icon-two")) { title.text("the"); }
-  else if(selected.hasClass("icon-three")) { title.text("team"); }
-  else if(selected.hasClass("icon-four")) { title.text("on"); }
-  else if(selected.hasClass("icon-five")) { title.text("your"); }
-  else if(selected.hasClass("icon-six")) { title.text("back"); }
-  else if(selected.hasClass("icon-seven")) { title.text("doe"); }
-  else { title.text("!!!"); }
-
-  // Inspiration
-  // Ideation
-  // Training
-  // Funding
-  // Mentoring
-  // Technology
-  // Impact
-  // Re-investing
+  if(selected.hasClass("icon-one")) { title.text("Inspiration"); }
+  else if(selected.hasClass("icon-two")) { title.text("Ideation"); }
+  else if(selected.hasClass("icon-three")) { title.text("Training"); }
+  else if(selected.hasClass("icon-four")) { title.text("Funding"); }
+  else if(selected.hasClass("icon-five")) { title.text("Mentoring"); }
+  else if(selected.hasClass("icon-six")) { title.text("Technology"); }
+  else if(selected.hasClass("icon-seven")) { title.text("Impact"); }
+  else { title.text("Re-investing"); }
 }
 
 function fadeOthers(selected, opacity) {
@@ -86,6 +73,8 @@ function fadeOthers(selected, opacity) {
 
 function fadeGirls() {
   $(".about-process-girls").animate({"opacity": 0}, function() {
+    $('.about-process-girl-left').attr('src', '/assets/daughter.png');
+    $('.about-process-girl-right').attr('src', '/assets/daughter.png');
     $(".about-process-girls").animate({"opacity": 1});
   });
 }
