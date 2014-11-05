7650337bc72030b0ee2be21efec3595b8ad34fe7
warrenshen <warrenzshen@gmail.com>
2014-11-05 11:53:22 -0800

    legit examples

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -1,6 +1,15 @@
 var ready = function() {
   $(document).ready(function() {
     $(".campaigns-create-second").hide();
+    campaignsChangeDescription("one");
+
+    $(".campaigns-create-field").focusin(function() {
+      $(this).css("border-color", "#FFC953");
+    });
+
+    $(".campaigns-create-field").focusout(function() {
+      $(this).css("border-color", "#CCCCCC");
+    });
   });
 }
 
@@ -10,4 +19,25 @@ $(document).on('page:load', ready);
 function campaignsCreateSecond() {
   $(".campaigns-create-first").hide();
   $(".campaigns-create-second").show();
+}
+
+function campaignsChangeDescription(which) {
+  if(which == "one") {
+    $("#campaigns-create-example-one").addClass("campaigns-create-example-colored");
+    $("#campaigns-create-example-two").removeClass("campaigns-create-example-colored");
+    $("#campaigns-create-example-custom").removeClass("campaigns-create-example-colored");
+    $("#campaign_description").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+  }
+  else if(which == "two") {
+    $("#campaigns-create-example-two").addClass("campaigns-create-example-colored");
+    $("#campaigns-create-example-one").removeClass("campaigns-create-example-colored");
+    $("#campaigns-create-example-custom").removeClass("campaigns-create-example-colored");
+    $("#campaign_description").text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.");
+  }
+  else {
+    $("#campaigns-create-example-custom").addClass("campaigns-create-example-colored");
+    $("#campaigns-create-example-one").removeClass("campaigns-create-example-colored");
+    $("#campaigns-create-example-two").removeClass("campaigns-create-example-colored");
+    $("#campaign_description").text("\n\n\nDon't drop the rock.");
+  }
 }
