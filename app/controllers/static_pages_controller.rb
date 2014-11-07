ab60fdf22192907df6e1beff97fe9ce996b96677
Charles Xue <charlesx2013@gmail.com>
2014-11-07 14:32:33 -0800

    hound lol

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,13 @@
 class StaticPagesController < ApplicationController
-  def index 
+  def index
   end
 
   def contact_email
-      UserMailer.contact_email(params[:contact_first], params[:contact_last], params[:contact_email], params[:contact_subject], params[:contact_message]).deliver
+      UserMailer.contact_email(params[:contact_first],
+                               params[:contact_last],
+                               params[:contact_email],
+                               params[:contact_subject],
+                               params[:contact_message]).deliver
       flash[:notice] = "Thanks for sending us an email. We'll get back to you soon!"
       redirect_to root_path
   end
