e1173837cfaec7f5a9ab683b25bfd114a749b235
Charles Xue <charlesx2013@gmail.com>
2014-11-10 03:59:50 -0800

    added update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+  before_filter :valid_user
+
   def edit
-    @user = User.find params[:id]
   end
 
   def update
+    if @user.update_with_password user_params
+      flash[:success] = "You've successfully updated your profile!"
+      redirect_to after_sign_in_path_for @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :picture,
+                                 :password_confirmation, :current_password)
+  end
+
+  def valid_user
+    @user = User.find params[:id]
+    unless current_user && current_user == @user
+      flash[:error] = "You can't access this page!"
+      redirect_to root_path
+    end
   end
 end
