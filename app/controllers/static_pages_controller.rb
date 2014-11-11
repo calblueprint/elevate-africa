bf81f10b94fabc308b49e276e64b7d33cf53953c
Tricia Fu <triciasfu@gmail.com>
2014-11-11 01:30:03 -0800

    campaign show general info style

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
   def contact_email
     UserMailer.contact_email(params[:contact_first],
                              params[:contact_last],
-                             params[:contact_email],
+                             params[:contact_email_address],
                              params[:contact_subject],
                              params[:contact_message]).deliver
     flash[:notice] = "Thanks for sending us an email. We'll get back to you soon!"
