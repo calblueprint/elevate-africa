9711b896c9a5e78b1b551171081f831674b560de
Charles Xue <charlesx2013@gmail.com>
2014-11-11 21:26:08 -0800

    moving things around

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :valid_user
+  before_filter :valid_user, except: :show
+
+  def show
+    @user = User.find params[:id]
+    @campaign = @user.campaign unless @user.admin?
+  end
 
   def edit
   end
