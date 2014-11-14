aa94a2bb746423a5a804c71aa4267dd7f554759d
Charles Xue <charlesx2013@gmail.com>
2014-11-13 23:53:30 -0800

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,21 @@
 #   cities = City.create([{ name: "Chicago" }, { name: "Copenhagen" }])
 #   Mayor.create(name: "Emanuel", city: cities.first)
 
+def create_admin_with_buzzes
+  1.times do |n|
+    admin = Admin.create email: "admin#{n}@gmail.com",
+                         password: "password",
+                         name: "Admin #{n}"
+  end
+  5 times do |n|
+    admin.buzzes.create headline: "This is a cool headline",
+                        subhead: "This is a cool subhead",
+                        description: Faker::Lorem.paragraph(8),
+                        box_size: Random.new.rand(1..3),
+                        box_color: "#FFC48C"
+  end
+end
+
 def create_admin
   1.times do |n|
     Admin.create email: "admin#{n}@gmail.com",
@@ -28,9 +43,7 @@ def create_team_and_campaign_with_donations
   end
 end
 
-def create_buzzes
-end
-
+create_admin_with_buzzes
 create_admin
 create_team_and_campaign_with_donations
 create_buzzes
