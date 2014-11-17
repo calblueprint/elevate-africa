9da706b0033efc66dc850a3652f09826beb89eca
Tricia Fu <triciasfu@gmail.com>
2014-11-16 20:36:26 -0800

    show page done except milestones

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -19,6 +19,7 @@ class Campaign < ActiveRecord::Base
   belongs_to :team
 
   has_many :donations, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   mount_uploader :picture, PictureUploader
 
