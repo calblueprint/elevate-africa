7bf662f515ab5ebd63cc2e280bd0fceabd185fb5
Charles Xue <charlesx2013@gmail.com>
2014-11-20 21:56:09 -0800

    adding things yay

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,11 +1,12 @@
 class DonationsController < ApplicationController
   def new
+    @campaign = Campaign.find_by id: params[:campaign]
     @donation = Donation.new
   end
 
   def create
-    @campaign = Campaign.find(params[:campaign_id])
-    @donation = @campaign.donations.build(donation_params)
+    @campaign = Campaign.find_by id: donation_params[:campaign_id]
+    @donation = Donations.new donation_params
     if @donation.save
       customer = Stripe::Customer.create email: params[:stripeEmail],
                                          card: params[:stripeToken]
