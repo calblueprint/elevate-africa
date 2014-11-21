770b98af096ad6ffc70a331bc4399a86e7d4b4ff
Kevin TianXiang Wu <kevintxwu@gmail.com>
2014-11-20 21:19:21 -0800

    pure lato

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110044328) do
+ActiveRecord::Schema.define(version: 20141110182844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,16 +75,6 @@ ActiveRecord::Schema.define(version: 20141110044328) do
   end
 
   add_index "donations", ["campaign_id"], name: "index_donations_on_campaign_id", using: :btree
-
-  create_table "people", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "subject"
-    t.string   "email"
-    t.string   "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
