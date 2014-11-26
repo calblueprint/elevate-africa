832ce6065644d3e96730d5bca45e8458ac119b10
warrenshen <warrenzshen@gmail.com>
2014-11-25 23:15:03 -0800

    safari and jeep workign

--- app/assets/javascripts/adventure.js
+++ app/assets/javascripts/adventure.js
@@ -1,16 +1,18 @@
 var ready = function() {
   $(document).ready(function() {
     // // adventure
-    $("#campaigns-adventure-foreground").click(function() {
+    $(".campaigns-adventure-foreground").click(function() {
       $("#campaigns-adventure-team").addClass("campaigns-adventure-team-hide");
       setTimeout(function() {
-        $("#campaigns-adventure-vehicle-body").addClass("campaigns-adventure-swing");
-        $("#campaigns-adventure-foreground").animate({"background-position-x": "-=400px"}, 3000);
-        $("#campaigns-adventure-background").animate({"background-position-x": "-=400px"}, 3000);
+        // $(".campaigns-adventure-vehicle-body").addClass("ca-animate-boat");
+        $(".campaigns-adventure-wheel").addClass("ca-animate-jeep");
+        $(".campaigns-adventure-foreground").animate({"background-position-x": "-=400px"}, 3000);
+        $(".campaigns-adventure-background").animate({"background-position-x": "-=400px"}, 3000);
         setTimeout(function() {
+          // $(".campaigns-adventure-vehicle-body").removeClass("ca-animate-boat");
+          $(".campaigns-adventure-wheel").removeClass("ca-animate-jeep");
           $("#campaigns-adventure-team").addClass("campaigns-adventure-team-show");
           $("#campaigns-adventure-team").removeClass("campaigns-adventure-team-hide");
-          $("#campaigns-adventure-vehicle-body").removeClass("campaigns-adventure-swing");
         }, 3000);
         setTimeout(function() {
           $("#campaigns-adventure-team").removeClass("campaigns-adventure-team-show");
