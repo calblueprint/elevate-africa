b523691c6afd552fb13850bd300e84d4160f414c
Kevin TianXiang Wu <kevintxwu@gmail.com>
2014-12-07 16:43:54 -0800

    contact page

--- app/assets/javascripts/campaign.js
+++ app/assets/javascripts/campaign.js
@@ -16,11 +16,6 @@ var tab_four = false;
 var example_open = 0;
 
 var ready_campaign = function() {
-  if ($("#campaign-banner").length > 0) {
-      $(document).ready(function() {
-          window.sr = new scrollReveal();
-      });
-  }
 }
 
 $(document).on('page:load', ready_campaign);
